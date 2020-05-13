fa462d2b1475d07ec49b4a72c30c94551d73d6ed
amahaya <babisko113@yahoo.fr>
2020-05-13 11:20:06 +0200

    conditions

---
+++ tuto eight ball.js
@@ -0,0 +1,77 @@
+let userName = 'Seth';
+//tenary expression
+userName ? return ('Hello, Seth!') : return ('Hello!');
+// A question the user wants to ask
+let userQuestions = 'what do you want?';
+return(`The user asked:${userQuestions}`);
+//Create another variable
+const randomNumber = Math.floor(Math.random() * 8);
+let eightBall = '';
+
+//conditional switch
+switch (eightBall){
+  case 0:
+  eightBall = 'It is certain';
+  break;
+  case 1:
+  eightBall = 'It is decidedly so';
+  break;
+  case 2:
+  eightBall = 'Reply hazy try again';
+  break;
+  case 3:
+  eightBall = 'Cannot predict now';
+  break;
+  case 4:
+  eightBall = 'Do not count on it';
+  break;
+  case 5:
+  eightBall = 'My sources say no';
+  break;
+  case 6:
+  eightBall = 'Outlook not so good';
+  break;
+  case 7:
+  eightBall = 'Signs point to yes';
+  break;
+  default:
+  return(`The eight ball answered: ${eightBall}`);
+}
+
+
+//conditional if....else
+if (eightBall = case 0){
+   return ('It is certain');
+ } else if (eightBall = case 1){
+   return ('It is decidedly so');
+ } else if (eightBall = case 2){
+   return ('Reply hazy try again');
+ } else if (eightBall = case 3){
+   return ('Cannot predict now');
+ } else if (eightBall = case 4){
+   return ('Do not count on it');
+ } else if (eightBall = case 5){
+   return ('My sources say no');
+ } else if (eightBall = case 6){
+   return ('Outlook not so good');
+ } else {
+   return ('Signs point to yes');
+ }
+
+if (eightBall = case 0) {
+  return "It is certain";
+} else if (eightBall = case 1) {
+  return "It is decidedly so";
+} else if (eightBall = case 2) {
+  return "Reply hazy try again";
+} else if (eightBall = case 3) {
+  return "Cannot predict now";
+} else if (eightBall = case 4) {
+  return "Do not count on it";
+} else if (eightBall = case 5) {
+  return "My sources say no";
+} else if (eightBall = case 6) {
+  return "Outlook not so good";
+} else {
+  return "Signs point to yes";
+}
